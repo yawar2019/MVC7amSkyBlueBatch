d2ae8c95e10bea9c5e77bebdb3f0327fcb05fdfe
yawar2019 <yawarali17@gmail.com>
2020-08-15 07:57:59 +0530

    published

--- CodeFirstApproach/Controllers/HomeController.cs
+++ CodeFirstApproach/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CodeFirstApproach.Models;
+using System.IO;
+
 namespace CodeFirstApproach.Controllers
 {
     public class HomeController : Controller
@@ -36,6 +38,70 @@ namespace CodeFirstApproach.Controllers
             {
                 return View();
             }
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            db.EmployeeModels.Remove(emp);
+
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult HtmlHelperExample()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpName = "Kat";
+
+
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase fileupload)
+        {
+            string filename = Path.GetFileName(fileupload.FileName);
+            string path = Server.MapPath("~/upload");
+            fileupload.SaveAs(Path.Combine(path, filename));
+            ViewBag.msg = "uploaded successfully";
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpName = "Kat";
+            return View("HtmlHelperExample",obj);
         }
 
     }
